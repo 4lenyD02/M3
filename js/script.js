89a2f03b0c610af16984837f50e68f28b31bf9cf
4lenyD02 <127409311+4lenyD02@users.noreply.github.com>
2023-06-22 02:39:07 -0600

    Agregar archivos

---
+++ js/script.js
@@ -0,0 +1,14 @@
+function scrollToSection() {
+    var section = document.getElementById("form");
+    section.scrollIntoView({behavior: "smooth"});
+  }
+
+
+  const spinnerWrapperEl = document.querySelector('.spinner-wrapper');
+  window.addEventListener('load', () => { 
+    spinnerWrapperEl.style.opacity ='0';
+
+  setTimeout(() => { 
+    spinnerWrapperEl.style.display= 'none'; 
+}, 200); 
+  }); 
